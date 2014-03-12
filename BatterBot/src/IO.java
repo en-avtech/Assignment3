bf58a4626c710f3eeebfd22ef980813884d723ee
en-avtech <en.avtech@gmail.com>
2014-03-12 13:11:20 -0700

    Initialization

---
+++ BatterBot/src/IO.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * Implementation of the IO interface.  It might even work.
+ * 
+ * @author Benjamin
+ */
+public class IO implements IOInterface
+{
+
+    public String read()
+    {
+        Scanner askUser = new Scanner(System.in);
+        System.out.print("Enter your answer: ");
+        
+        String read = askUser.next();
+        return read;
+    }
+    
+    public void print (String response)
+    {
+        System.out.println(response + "\n");
+    }
+}
